9b398def69fa2c4e702fcddfc0eeeebf682a55b8
oneameeeena <aminaouaqqaouraho@gmail.com>
2024-08-01 06:50:11 -0700

    LIBDY

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,11 +1,28 @@
 import random
 import ctypes
 
-cops = ctypes.CDLL('100-operations.so')
+# Load the shared library
+cops = ctypes.CDLL('./100-operations.so')
+
+# Generate random integers
 a = random.randint(-111, 111)
 b = random.randint(-111, 111)
+
+# Ensure the functions are correctly typed
+cops.add.restype = ctypes.c_int
+cops.sub.restype = ctypes.c_int
+cops.mul.restype = ctypes.c_int
+cops.div.restype = ctypes.c_int
+cops.mod.restype = ctypes.c_int
+
 print("{} + {} = {}".format(a, b, cops.add(a, b)))
 print("{} - {} = {}".format(a, b, cops.sub(a, b)))
 print("{} x {} = {}".format(a, b, cops.mul(a, b)))
-print("{} / {} = {}".format(a, b, cops.div(a, b)))
-print("{} % {} = {}".format(a, b, cops.mod(a, b)))
+
+# Check for division by zero
+if b != 0:
+    print("{} / {} = {}".format(a, b, cops.div(a, b)))
+    print("{} % {} = {}".format(a, b, cops.mod(a, b)))
+else:
+    print("Division and modulo by zero are not defined.")
+
